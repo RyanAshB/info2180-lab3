5d1596a7463b69c07c52d2a8806b40f8c9192fe0
RyanAshB <ryanash.b@gmail.com>
2023-10-22 22:27:40 -0500

    Exercise 2 Complete

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,9 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gridSquares = document.querySelectorAll('#board div')
+    const gameBoard = ["", "", "", "", "", "", "", "", ""];
 
     gridSquares.forEach(square => {
         square.classList.add('square')
     })
 
-    
+    gridSquares.forEach((square, index) => {
+        square.addEventListener('click', () => {
+
+            //Checks if the current square already has a value
+            if (gameBoard[index] == "") {
+                const numberOfMoves = gameBoard.filter(symbol => symbol !== "").length;
+
+                // Determine the current player (X or O) based on the number of moves
+                let currentPlayer;
+                if (numberOfMoves % 2 === 0) {
+                  currentPlayer = 'X'; // Even number of moves, so it's player X's turn
+                } else {
+                  currentPlayer = 'O'; // Odd number of moves, so it's player O's turn
+                }
+
+                // Update the game state and display the X or O in the square
+                gameBoard[index] = currentPlayer;
+                square.classList.add(currentPlayer);
+                square.textContent = currentPlayer;
+            }
+        })
+    })
+
+
 });
